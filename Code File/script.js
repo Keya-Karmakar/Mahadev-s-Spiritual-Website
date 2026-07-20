7fe376490f27bc6811c8f471a38e348632b62b4b
Keya Karmakar <keya12376@gmail.com>
2026-07-21 05:23:35 +1200

    Add files via upload

---
+++ Code File/script.js
@@ -0,0 +1,14 @@
+const quotes=[
+
+"Meditation is the path to Mahadev.",
+
+"Har Har Mahadev.",
+
+"Shiva destroys ignorance.",
+
+"Silence is the language of Shiva.",
+
+"Faith is the greatest strength."
+
+];
+document.getElementById("q").innerHTML=quotes[Math.floor(Math.random()*quotes.length)];
